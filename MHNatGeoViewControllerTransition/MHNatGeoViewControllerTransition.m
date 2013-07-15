dc7f6c491eed51b5c67649b7dbfd9d7547980a39
Michael henry Pantaleon <michaelhenry119@yahoo.com.ph>
2013-07-15 20:04:48 +0800

    reset anchor point after dismissing.
    Signed-off-by: Michael henry Pantaleon <michaelhenry119@yahoo.com.ph>

--- MHNatGeoViewControllerTransition/MHNatGeoViewControllerTransition.m
+++ MHNatGeoViewControllerTransition/MHNatGeoViewControllerTransition.m
@@ -56,17 +56,21 @@
         sourceLastTransform([_sourceView layer]);
         destinationLastTransform([_destinationView layer]);
         
+        
          //Perform animation
         [UIView animateWithDuration:0.8f*_duration delay:0.2f*_duration options:UIViewAnimationOptionCurveLinear animations:^{
             destinationFirstTransform([_destinationView layer]);
         } completion:^(BOOL finished) {
             completion(YES);
+            
         }];
         
         [UIView animateWithDuration:_duration delay:0.0f options:0 animations:^{
             sourceFirstTransform([_sourceView layer]);
         } completion:^(BOOL finished) {
-        
+            CGRect oldFrame = [[_sourceView layer]frame];
+            [[_sourceView layer]setAnchorPoint:CGPointMake(0.5,0.5f)];
+            [[_sourceView layer] setFrame:oldFrame];
         }];
     }else {
         // Change anchor point and reposition it.
